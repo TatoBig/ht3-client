148d69a0a7bc28c1bdaf1519c7b249dad488113d
tatobig <tatobig@gmail.com>
2023-05-26 00:35:50 +0000

    Finished app for 2 databases

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
-import { PrismaClient } from '@prisma/client'
-import { faker } from '@faker-js/faker';
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-
-const prisma = new PrismaClient()
+const client1 = new PrismaClient({
+  datasources: { db: { url: process.env.DB1_URL } },
+});
+const client2 = new PrismaClient({
+  datasources: { db: { url: process.env.DB2_URL } },
+});
 
 function sleep(ms: number) {
   return new Promise((resolve) => {
@@ -19,26 +23,34 @@ function sleep(ms: number) {
 }
 
 async function main() {
-  while(true) {
-    await sleep(1000)
-    await prisma.user.create({
+  while (true) {
+    await sleep(1000);
+    await client1.user.create({
       data: {
         email: faker.internet.email(),
-        name: faker.person.firstName()
-      }
-    })  
+        name: faker.person.firstName(),
+      },
+    });
+    await client2.user.create({
+      data: {
+        email: faker.internet.email(),
+        name: faker.person.firstName(),
+      },
+    });
   }
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await client1.$disconnect();
+    await client2.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await client1.$disconnect();
+    await client2.$disconnect();
+    process.exit(1);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
